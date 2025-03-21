2b9ab30c282951aae38eebd8df14c6bd0fd78bc1
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-03-21 15:49:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Parser krajowej tablicy przeznaczeń częstotliwości
+---
+
+Oczywiście, że nie przepiszę całego bandplanu ręcznie. Jednak przy automatycznym przetwarzaniu można natknąć się na zaskakująco dużo wyzwań. Cała tabela jest zbudowana w sposób który utrudnia automatyczne rozpoznanie wierszy. Największym zaskoczeniem było, że strona 40 została "załatana" jako obraz, a nie tekst. Tą stronę trzeba przepisać ręcznie. Dodatkowo na stronie 36, Lp. 341 ma przypis który myli parser.
+
+Stan na listopad 2023. Nie dodałem jeszcze nowelizacji z lipca 2024 (nie uwzględniona w tekście jednolitym).
---
+++ parse.py
@@ -0,0 +1,20 @@
+import os
+import camelot
+
+# NOTE: Manually download "https://isap.sejm.gov.pl/isap.nsf/download.xsp/WDU20240001141/O/D20241141.pdf" and save as "source.pdf"
+pdf_filename = "source.pdf"
+output_dir = "output"
+
+os.makedirs(output_dir, exist_ok=True)
+
+if not os.path.exists(pdf_filename):
+    print(f"File {pdf_filename} not found")
+    exit(1)
+
+# Extract tables and save as markdown
+for i in range(3, 87):
+    tables = camelot.read_pdf(pdf_filename, pages=str(i), flavor='stream')
+    if tables:
+        tables[0].to_markdown(os.path.join(output_dir, f'page_{i}.md'))
+    else:
+        print(f'No tables found on page {i}')
